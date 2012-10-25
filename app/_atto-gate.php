4c23b1db7a0ab5e5d574d7646e435a86067f308e
nulil <sippotuki@gmail.com>
2012-10-25 21:56:06 +0900

    　

--- app/_atto-gate.php
+++ app/_atto-gate.php
@@ -17,19 +17,19 @@
  * @license		MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 /*
- * __gate.php
+ * _atto-gate.php
  *
  * The Front Controller to handle request
  */
 
-require dirname( __FILE_ ) . '/../atto/atto.php';
+require dirname( __FILE__ ) . '/../atto/atto.php';
 Atto::cascade( 'app_root', array(
 	'admin_teimezone'	 => 'Asia/Tokyo',
 	'logging_level'		 => 4, // 0:off/1:error only/2:warning or error/3:more info/4:all
 	'logging_MiBbyte'	 => 2,
 	'logging_files'		 => array( 'log1.txt', 'log2.txt', 'log3.txt' ),
-	'root_uri'	 => '../..',
-	'htdocs_uri' => '../..',
+	'root_uri'	 => '..',
+	'htdocs_uri' => '..',
 	'app_uri'	 => '',
 ) );
 
